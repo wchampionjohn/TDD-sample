892d7349e9d6017ad9ed28762e7f8e30a4e5d8c0
john.hsieh <john.hsieh@rockmobile.com>
2016-10-28 11:24:00 +0800

    [add]新增並通過了輸入值是11 12 15 17的test case

--- spec/changer_spec.rb
+++ spec/changer_spec.rb
@@ -10,6 +10,11 @@ describe 'Coin changer' do
     [6, [5, 1]],
     [7, [5, 1, 1]],
     [10, [10]],
+    [10, [10]],
+    [11, [10, 1]],
+    [12, [10, 1, 1]],
+    [15, [10, 5]],
+    [17, [10, 5, 1, 1]],
   ].each do |input, expectation|
     it "shuold return #{expectation} if gvien #{input}" do
       expect(Changer.change(input)).to eq expectation
