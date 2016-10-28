0cfdefab8f47997f8b200bafcff5c20d25997f3f
john.hsieh <john.hsieh@rockmobile.com>
2016-10-28 10:32:58 +0800

    [add]新增了輸入值是5的test case

--- spec/changer_spec.rb
+++ spec/changer_spec.rb
@@ -6,6 +6,7 @@ describe 'Coin changer' do
     [1, [1]],
     [2, [1, 1]],
     [3, [1, 1, 1]],
+    [5, [5]],
   ].each do |input, expectation|
     it "shuold return #{expectation} if gvien #{input}" do
       expect(Changer.change(input)).to eq expectation
