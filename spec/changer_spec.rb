5cd7b1e68cf85a6f9c9b8161a82368d0c88165f2
john.hsieh <john.hsieh@rockmobile.com>
2016-10-28 10:29:30 +0800

    [refactor]重構測試

--- spec/changer_spec.rb
+++ spec/changer_spec.rb
@@ -2,15 +2,13 @@ require 'rspec'
 require_relative '../lib/Changer'
 
 describe 'Coin changer' do
-  it 'shuold return 1 if gvien 1' do
-    expect(Changer.change(1)).to eq [1]
-  end
-
-  it 'shuold return [1,1] if gvien 2' do
-    expect(Changer.change(2)).to eq [1, 1]
-  end
-
-  it 'shuold return [1, 1, 1] if gvien 3' do
-    expect(Changer.change(3)).to eq [1, 1, 1]
+  [
+    [1, [1]],
+    [2, [1, 1]],
+    [3, [1, 1, 1]],
+  ].each do |input, expectation|
+    it "shuold return #{expectation} if gvien #{input}" do
+      expect(Changer.change(input)).to eq expectation
+    end
   end
 end
