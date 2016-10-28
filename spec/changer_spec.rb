55478486988e83490f82f977db94d9f8254fadc7
john.hsieh <john.hsieh@rockmobile.com>
2016-10-28 10:20:20 +0800

    [add]新增了輸入值是2的test case

--- spec/changer_spec.rb
+++ spec/changer_spec.rb
@@ -5,4 +5,8 @@ describe 'Coin changer' do
   it 'shuold return 1 if gvien 1' do
     expect(Changer.change(1)).to eq [1]
   end
+
+  it 'shuold return 2 if gvien 1' do
+    expect(Changer.change(2)).to eq [1, 1]
+  end
 end
