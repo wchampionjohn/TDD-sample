6ad1e15821e54c9daeacb756188da37e366620a1
john.hsieh <john.hsieh@rockmobile.com>
2016-10-28 10:33:56 +0800

    [add]通過了輸入值是5的test case

--- lib/changer.rb
+++ lib/changer.rb
@@ -1,6 +1,12 @@
 class Changer
   def self.change amount
     result = []
+
+    if amount >= 5
+      result << 5
+      amount -= 5
+    end
+
     while amount > 0 do
       result << 1
       amount -= 1
